bb8355c9ce910a104aa53694660ba6b256716919
Jason Granzow <jason.granzow@gmail.com>
2024-08-07 18:25:44 -0500

    fixed issues with negative modulus that were leading to incorrect crypto.

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,11 @@ pub fn encrypt(input: &str, key: isize) -> String {
     let modified_string: String = input.chars()
         .map(|ch: char|
             if ch.is_ascii_uppercase() {
-                ((ch as isize - 'A' as isize + key) % 26 + 'A' as isize) as u8 as char
+                ((ch as isize - 'A' as isize + key).rem_euclid(26) + 'A' as isize) as u8 as char
             } else if ch.is_ascii_lowercase() {
-                ((ch as isize - 'a' as isize + key) % 26 + 'a' as isize) as u8 as char
+                ((ch as isize - 'a' as isize + key).rem_euclid(26) + 'a' as isize) as u8 as char
             } else if ch.is_ascii_digit() {
-                ((ch as isize - '0' as isize + key) % 10 + '0' as isize) as u8 as char
+                ((ch as isize - '0' as isize + key).rem_euclid(10) + '0' as isize) as u8 as char
             } else {
                 ch as u8 as char
             }
@@ -19,20 +19,11 @@ pub fn decrypt(input: &str, key: isize) -> String {
     let modified_string: String = input.chars()
     .map(|ch|
         if ch.is_ascii_uppercase() {
-            ((ch as isize - 'A' as isize - key + 26) % 26 + 'A' as isize) as u8 as char
+            ((ch as isize - 'A' as isize - key).rem_euclid(26) + 'A' as isize) as u8 as char
         } else if ch.is_ascii_lowercase() {
-            ((ch as isize - 'a' as isize - key + 26) % 26 + 'a' as isize) as u8 as char
+            ((ch as isize - 'a' as isize - key).rem_euclid(26) + 'a' as isize) as u8 as char
         } else if ch.is_ascii_digit() {
-            let zero = '0' as isize;
-            let mut ansr = ch as isize - zero;
-            ansr = ansr - key;
-            ansr = ansr + 10;
-            ansr = ansr % 10;
-            // ansr = ansr.abs();
-            ansr = ansr + zero;
-            let ansr8 = ansr as u8;
-            let ansrchar = ansr8 as char;
-            ((ch as isize - '0' as isize - key + 10) % 10 + '0' as isize) as u8 as char
+            ((ch as isize - '0' as isize - key).rem_euclid(10) + '0' as isize) as u8 as char
         } else {
             ch
         }
@@ -46,23 +37,51 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_encryption() {
-        // Short test
+    fn test_encrypt_short() {
         let result = encrypt("abc", 3);
         assert_eq!(result, "def");
+    }
+    
+    #[test]
+    fn test_encrypt_short_negative() {
+        let result = encrypt("abc", -3);
+        assert_eq!(result, "xyz");
+    }
 
-        // Long string test
+    #[test]
+    fn test_encrypt_long() {
         let msg =    "If he had anything confidential to say, he wrote it in cipher, that is, by so changing the order of the letters of the alphabet, that not a word could be made out.";
         let cipher = "Pm ol ohk hufaopun jvumpkluaphs av zhf, ol dyval pa pu jpwoly, aoha pz, if zv johunpun aol vykly vm aol slaalyz vm aol hswohila, aoha uva h dvyk jvbsk il thkl vba.";
         let result = encrypt(msg, 59);
-        assert_eq!(result, cipher); 
+        assert_eq!(result, cipher);
+    }
 
-        // Number test
+    #[test]
+    fn test_encrypt_long_negative() {
+        let msg =    "If he had anything confidential to say, he wrote it in cipher, that is, by so changing the order of the letters of the alphabet, that not a word could be made out.";
+        let cipher = "By ax atw tgrmabgz vhgybwxgmbte mh ltr, ax pkhmx bm bg vbiaxk, matm bl, ur lh vatgzbgz max hkwxk hy max exmmxkl hy max teiatuxm, matm ghm t phkw vhnew ux ftwx hnm.";
+        let result = encrypt(msg, -59);
+        assert_eq!(result, cipher); 
+    }
+
+    #[test]
+    fn test_encrypt_numbers() {
         let msg = "1234567890";
         let cipher = "4567890123";
         let result = encrypt(msg, 3);
         assert_eq!(result, cipher);
+    }
 
+    #[test]
+    fn test_encrypt_numbers_negative() {
+        let msg = "1234567890";
+        let cipher = "8901234567";
+        let result = encrypt(msg, -3);
+        assert_eq!(result, cipher);
+    }
+
+    #[test]
+    fn test_encrypt_mixed() {
         // Mixed test
         let msg = "a1b2c3d4e5f6g7h8i9j0";
         let cipher = "x4y5z6a7b8c9d0e1f2g3";
@@ -71,28 +90,103 @@ mod tests {
     }
 
     #[test]
-    fn test_decryption() {
-        // // Short string test
-        // let result = decrypt("def", 3);
-        // assert_eq!(result, "abc");
+    fn test_encrypt_mixed_negative() {
+        // Mixed test
+        let msg = "a1b2c3d4e5f6g7h8i9j0";
+        let cipher = "d8e9f0g1h2i3j4k5l6m7";
+        let result = encrypt(msg, -23);
+        assert_eq!(result, cipher);
+    }
 
-        // // Long string test
-        // let msg =    "If he had anything confidential to say, he wrote it in cipher, that is, by so changing the order of the letters of the alphabet, that not a word could be made out.";
-        // let cipher = "Pm ol ohk hufaopun jvumpkluaphs av zhf, ol dyval pa pu jpwoly, aoha pz, if zv johunpun aol vykly vm aol slaalyz vm aol hswohila, aoha uva h dvyk jvbsk il thkl vba.";
-        // let result = decrypt(cipher, 7);   
-        // assert_eq!(result, msg); 
+    #[test]
+    fn test_encrypt_large_key() {
+        let msg = "a1b2c3d4e5f6g7h8i9j0";
+        let cipher = "w7x8y9z0a1b2c3d4e5f6";
+        let result = encrypt(msg, 8_446_744_073_709_551_606);
+        assert_eq!(result, cipher);
+    }
 
-        // Number test
+    #[test]
+    fn test_encrypt_large_key_negative() {
+        let msg = "a1b2c3d4e5f6g7h8i9j0";
+        let cipher = "e5f6g7h8i9j0k1l2m3n4";
+        let result = encrypt(msg, -8_446_744_073_709_551_606);
+        assert_eq!(result, cipher);
+    }
+
+    #[test]
+    fn test_decryption_short() {
+        let result = decrypt("def", 3);
+        assert_eq!(result, "abc");
+    }
+
+    #[test]
+    fn test_decryption_short_negative() {
+        let result = decrypt("def", -3);
+        assert_eq!(result, "ghi");
+    }
+
+    #[test]
+    fn test_decryption_long() {
+        let msg =    "If he had anything confidential to say, he wrote it in cipher, that is, by so changing the order of the letters of the alphabet, that not a word could be made out.";
+        let cipher = "Pm ol ohk hufaopun jvumpkluaphs av zhf, ol dyval pa pu jpwoly, aoha pz, if zv johunpun aol vykly vm aol slaalyz vm aol hswohila, aoha uva h dvyk jvbsk il thkl vba.";
+        let result = decrypt(cipher, 33);   
+        assert_eq!(result, msg); 
+    }
+
+    #[test]
+    fn test_decryption_long_negative() {
+        let msg =    "If he had anything confidential to say, he wrote it in cipher, that is, by so changing the order of the letters of the alphabet, that not a word could be made out.";
+        let cipher = "By ax atw tgrmabgz vhgybwxgmbte mh ltr, ax pkhmx bm bg vbiaxk, matm bl, ur lh vatgzbgz max hkwxk hy max exmmxkl hy max teiatuxm, matm ghm t phkw vhnew ux ftwx hnm.";
+        let result = decrypt(cipher, -33);   
+        assert_eq!(result, msg); 
+    }
+
+    #[test]
+    fn test_decryption_numbers() {
         let msg = "1234567890";
         let cipher = "4567890123";
-        let result = decrypt(cipher, 23);
+        let result = decrypt(cipher, 53);
         assert_eq!(result, msg);
+    }
 
-        // // Mixed test
-        // let msg = "a1b2c3d4e5f6g7h8i9j0";
-        // let cipher = "b2c3d4e5f6g7h8i9j0k1";    // 27
-        // // let cipher = "x4y5z6a7b8c9d0e1f2g3";    // 23
-        // let result = decrypt(cipher, 27);
-        // assert_eq!(result, msg);
+    #[test]
+    fn test_decryption_numbers_negative() {
+        let msg = "1234567890";
+        let cipher = "8901234567";
+        let result = decrypt(cipher, -53);
+        assert_eq!(result, msg);
+    }
+
+    #[test]
+    fn test_decryption_mixed() {
+        let msg = "a1b2c3d4e5f6g7h8i9j0";
+        let cipher = "b8c9d0e1f2g3h4i5j6k7";
+        let result = decrypt(cipher, 27);
+        assert_eq!(result, msg);
+    }
+
+    #[test]
+    fn test_decryption_mixed_negative() {
+        let msg = "a1b2c3d4e5f6g7h8i9j0";
+        let cipher = "z4a5b6c7d8e9f0g1h2i3";
+        let result = decrypt(cipher, -27);
+        assert_eq!(result, msg);
+    }
+
+    #[test]
+    fn test_decrypt_large_key() {
+        let msg = "a1b2c3d4e5f6g7h8i9j0";
+        let cipher = "w7x8y9z0a1b2c3d4e5f6";
+        let result = decrypt(cipher, 8_446_744_073_709_551_606);
+        assert_eq!(result, msg);
+    }
+
+    #[test]
+    fn test_decrypt_large_key_negative() {
+        let msg = "a1b2c3d4e5f6g7h8i9j0";
+        let cipher = "e5f6g7h8i9j0k1l2m3n4";
+        let result = decrypt(cipher, -8_446_744_073_709_551_606);
+        assert_eq!(result, msg);
     }
 }
